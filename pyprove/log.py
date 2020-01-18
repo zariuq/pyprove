f675331c04120b003ecc96cb04df0fa923155d0c
Jan Jakubuv <jakubuv@gmail.com>
2020-01-18 15:11:17 +0100

    Improved logging methods

--- pyprove/log.py
+++ pyprove/log.py
@@ -49,3 +49,20 @@ def humanbytes(b):
       n += 1
    return "%.2f %s" % (b, units[n])
 
+def humanint(n):
+   s = str(int(abs(n)))
+   r = s[-3:]
+   s = s[:-3]
+   while s:
+      r = s[-3:] + "," + r
+      s = s[:-3]
+   return r if n >= 0 else "-%s" % r
+
+def humantime(s):
+   h = s // 3600
+   s -= 3600*h
+   m = s // 60
+   s -= 60*m
+   return "%02d:%02d:%04.1f" % (h,m,s)
+
+
